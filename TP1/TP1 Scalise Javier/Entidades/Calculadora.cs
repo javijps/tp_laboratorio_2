e49360700d082fe3739cc7810e815e3d3c743f8c
unknown <javijps@gmail.com>
2020-04-19 00:19:55 -0300

    preparado

---
+++ TP1/TP1 Scalise Javier/Entidades/Calculadora.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+
+        double Operar(Numero numero1,Numero numero2,string operador)
+        {
+            return 0;
+        }
+
+        private static string ValidarOperador(string operador)
+        {
+            return "+";
+        }
+    }
+}
---
+++ TP1/TP1 Scalise Javier/Entidades/Numero.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class Numero
+    {
+        private double numero;
+
+        #region Constructor
+
+        public Numero()
+        {
+            this.numero = 0;
+            new Numero();
+        }
+
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Numero(string numero)
+        {
+            this.numero = Convert.ToDouble(numero);
+        }
+
+        public double ValidarNumero(string strNumero)
+        {
+            return 0;
+        }
+
+        #endregion
+
+        #region Propiedades
+
+        public double SetNumero
+        {
+            set { this.numero = value; }
+        }
+
+        #endregion
+
+    }
+}
